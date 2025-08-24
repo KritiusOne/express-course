4421ddd8aa41d50dcdfcdf05039ede00bb11d470
Julio Rodriguez Ureche <julio10489@gmail.com>
2025-08-24 17:14:54 -0500

    feat: test post verb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import { CONFIG } from "./config/config.js";
+import bodyParser from "body-parser";
 const app = express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 const PORT = CONFIG.PORT || 3000
 
@@ -9,6 +12,48 @@ app.get("/", (req, res)=>{
     <h1>Express.js course</h1>
     <p>Running in port ${PORT}</p>
   `);
+});
+
+app.get("/users/:id", (req, res)=> {
+  const userId = req.params.id;
+  res.send(`Show user information with id ${userId}`)
+})
+
+app.get("/search", (req, res)=>{
+  const terms = req.query.tag || "";
+  const category = req.query.category || "All";
+  res.send(`
+    <h2>Search result: </h2>
+    <p>Tag: ${terms} </p>
+    <p>category: ${category} </p>
+  `)
+})
+
+app.post("/form", (req, res)=>{
+  const name = req.body.name || "John Doe";
+  const email = req.body.email || "";
+
+  res.json({
+    message: "Received data",
+    data: {
+      name, email
+    }
+  });
+
+});
+
+app.post("/api/data", (req, res)=>{
+  const data = req.body;
+  console.log(req.body)
+
+  if(!data || !Object.keys(data).length){
+    return res.status(400).json({ error: "Data no received" });
+  }
+
+  res.status(200).json({
+    message: "Data received successfully",
+    data
+  })
 });
 
 app.listen(PORT, ()=>{
